fe8b9ad530e9166a80bf79b322da20b918ba2120
Youssef Zahi <youssef.zahi2016@gmail.com>
2023-05-17 09:45:06 +0100

    Implement get single product route

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -10,7 +10,19 @@ const getAllProducts = async (req, res) => {
   res.status(StatusCodes.OK).json({ products });
 };
 
+const getSingleProduct = async (req, res) => {
+  const { id: productId } = req.params;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+  }
+  res.status(StatusCodes.OK).json({ product });
+};
+
 module.exports = {
   createProduct,
   getAllProducts,
+  getSingleProduct,
 };
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const {
   createProduct,
   getAllProducts,
+  getSingleProduct,
 } = require("../controllers/productController");
 
 router.route("/").post(createProduct).get(getAllProducts);
+router.route("/:id").get(getSingleProduct);
 module.exports = router;
